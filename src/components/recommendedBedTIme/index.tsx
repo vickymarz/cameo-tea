3a011543b8cb44373ce5b9195d9ba34a8c8231b2
vickymarz <okorojiebube2@gmail.com>
2023-02-10 09:03:57 +0100

    Add recommended sleep time

---
+++ src/components/recommendedBedTIme/index.tsx
@@ -0,0 +1,10 @@
+export const RecommendedBedTime = ({time}:{time:React.ReactNode}) => {
+    return (
+      <div className='basis-2/4 bg-[#2e2e33] shadow-cardShadow rounded-[12px] py-[1.25rem] px-[0.5rem] flex flex-col justify-center items-center gap-y-[1rem]'>
+        <h2 className='text-[#fff] text-[1.125rem] font-bold'>Avg. Sleep TIme</h2>
+        <div className='flex justify-center flex-col items-center gap-y-[0.5rem] text-[#a6ffd3]'>
+          <p className='font-bold text-[2.25rem]'>{time}</p>
+        </div>
+      </div>
+    )
+  }
--- src/pages/therapy/index.tsx
+++ src/pages/therapy/index.tsx
@@ -1,43 +1,41 @@
-import { useState, useEffect } from 'react'
-import { AverageSleepTime, Header, Timer, SleepEfficiency, Feedback } from "../../components"
+import { useState } from 'react'
+import { AverageSleepTime, Header, TimePicker, SleepEfficiency, Feedback } from "../../components"
 // import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import moment from 'moment';
 
 export const Homepage = () => {
-  const [value1, setValue1] = useState(moment());
-  const [value2, setValue2] = useState(moment());
-  const [value3, setValue3] = useState(moment());
-  const [average, setAverage] = useState(0)
-  const [efficiency, setEfficiency] = useState(0)
+  const [wakeTime, setWakeTime] = useState(moment());
+//   const [average, setAverage] = useState(0)
+//   const [efficiency, setEfficiency] = useState(0)
 
-  useEffect(() => {
-    const averageSleepTime = () => {
-      const sleepTime = moment(value2)
-      const wakeTime = moment(value3)
-      const difference = wakeTime.diff(sleepTime, 'hours')
-      setAverage(difference)
-    }
-    averageSleepTime()
+//   useEffect(() => {
+//     const averageSleepTime = () => {
+//       const sleepTime = moment(value2)
+//       const wakeTime = moment(value3)
+//       const difference = wakeTime.diff(sleepTime, 'hours')
+//       setAverage(difference)
+//     }
+//     averageSleepTime()
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value3])
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [value3])
 
-  useEffect(() => {
-    const sleepEfficiency = () => {
-      const bedTime = moment(value1)
-      const sleepTime = moment(value2)
-      const difference = sleepTime.diff(bedTime, 'hours')
-      const result = (average/difference) * 100
-      if(Number.isNaN(result)) {
-        setEfficiency(0)
-      } else {
-        setEfficiency(result)
-      }
-    }
-    sleepEfficiency()
+//   useEffect(() => {
+//     const sleepEfficiency = () => {
+//       const bedTime = moment(value1)
+//       const sleepTime = moment(value2)
+//       const difference = sleepTime.diff(bedTime, 'hours')
+//       const result = (average/difference) * 100
+//       if(Number.isNaN(result)) {
+//         setEfficiency(0)
+//       } else {
+//         setEfficiency(result)
+//       }
+//     }
+//     sleepEfficiency()
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value3, value1, value2])
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [value3, value1, value2])
 
   return (
     <div className="min-h-full w-full bg-[#242426] px-[16px]">
@@ -45,14 +43,14 @@ export const Homepage = () => {
         <div className="pt-[30px] pb-[32px]">
           <h2 className="text-[1.5rem] font-bold text-[#fff]">Find your next ideal bed time</h2>
         </div>
-        <Timer
-          title='When did you go to bed?'
-          value={value1}
-          setValue={setValue1}
+        <TimePicker
+          title='When do you wake up everyday?'
+          value={wakeTime}
+          setValue={setWakeTime}
         />
         <div className='flex justify-between gap-x-[10px]'>
-          <AverageSleepTime average={average}/>
           <SleepEfficiency efficiency={efficiency}/>
+          
         </div>
         <Feedback />
     </div>
